83bca988d63836d90312734c9c7eeeaf3c6eaf3f
Haniel Nunes Pereira Pinheiro <haniel_pereira@hotmail.com>
2023-08-11 21:22:16 -0300

    Ah

---
+++ forca/forca.h
@@ -0,0 +1,11 @@
+#define TAMANHO_PALAVRA 20
+
+int enforcou();
+int ganhou();
+int jachutou(char letra);
+void abertura();
+void chuta();
+void desenhaforca();
+void escolhepalavra();
+void end();
+void adicionapalavra();
